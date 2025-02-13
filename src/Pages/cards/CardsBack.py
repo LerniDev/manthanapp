cc5b76e0de2243076842ce5aa79c7007da653699
Baguette365 <baguette.berladin@gmail.com>
2025-02-13 23:03:07 +0100

    update the project

--- src/Pages/cards/CardsBack.py
+++ src/Pages/cards/CardsBack.py
@@ -1,16 +1,27 @@
 import flet as ft
-
+import json
 Decks={
     "Default": {},
-    "French": {},
 }
 
 class Cards(ft.Column):
-    def __init__(self, front, back, page: ft.Page):
+    def __init__(self, front, back, page: ft.Page, deck):
         super().__init__()
         self.page=page
+        Decks[deck][front]=self
         self.controls=[front, back]
+
+
+        try:
+            Decks[deck][front] = self
+        except:
+            Decks[deck]={}
+            Decks[deck][front] = self
 
     def read(self):
         self.page.clean()
         self.page.add(self)
+
+def updateDecksJson():
+    with open("decks.json", "w", encoding="utf-8") as f:
+        json.dump(Decks, f, indent=4)
--- src/Pages/cards/CardsMenu.py
+++ src/Pages/cards/CardsMenu.py
@@ -8,18 +8,46 @@ def cardsmenu(page: ft.Page):
     CreateViewfront        = ft.TextField(label="Front", hint_text="Ex: when jesus was born")
     CreateViewback         = ft.TextField(label="Back", hint_text="Ex: in year 0")
     CreateViewButton       = ft.CupertinoFilledButton("Create")
+    CreateViewSwitch       = ft.Switch(value=True)
+    CreateViewLabel        = ft.Text("create Card")
+    CreateViewName         = ft.TextField(label="Name", hint_text="Science Deck")
     CreateViewDeckSelector = ft.Dropdown(options=[ft.dropdown.Option(value) for value in CardsBack.Decks])
+
     def CreateCard():
         pass
 
     def ChangePage(index):
         if index==0:
             page.clean()
-            page.add(
-                CreateViewfront,
-                CreateViewback,
-                CreateViewButton,
-                CreateViewDeckSelector,
+            page.add(CreateViewSwitch)
+
+            if CreateViewSwitch.value==False:
+                CreateViewLabel.value = "create deck"
+                if CreateViewSwitch.value == True:
+
+                    CreateViewLabel.value = "create Card"
+                    page.add(
+                        CreateViewLabel,
+                        CreateViewfront,
+                        CreateViewback,
+                        CreateViewButton,
+                        CreateViewDeckSelector,
+                    )
+                page.add(CreateViewName)
+
+            if CreateViewSwitch.value==True:
+
+                if CreateViewSwitch == False:
+                    CreateViewLabel.value = "create deck"
+                    page.add(CreateViewName)
+
+                CreateViewLabel.value = "create Card"
+                page.add(
+                    CreateViewLabel,
+                    CreateViewfront,
+                    CreateViewback,
+                    CreateViewButton,
+                    CreateViewDeckSelector,
             )
 
         if index==1:
